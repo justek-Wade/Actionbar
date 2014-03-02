34d908c0cbf851aeac3054ed0f1b60e987689358
huangyulu <408975300@qq.com>
2014-03-02 17:40:35 +0800

    set a image resource

--- ViewPagerAndTabDemo/src/fragment/PictureFragment.java
+++ ViewPagerAndTabDemo/src/fragment/PictureFragment.java
@@ -6,18 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView.FindListener;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.example.viewpagerandtabdemo.R;
 
 public class PictureFragment extends SherlockFragment{
 
-	Button picturebtn;
+	private Button picturebtn;
+	private ImageView image;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View contextView = inflater.inflate(R.layout.picturefragment, container, false);
 		picturebtn=(Button) contextView.findViewById(R.id.picturebtn);
+	
+		image=(ImageView) contextView.findViewById(R.id.image);
+		image.setImageResource(R.drawable.shendun);
 		return contextView;
 	}
 }
